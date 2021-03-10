2581b5e8c56b5d5228c658e78f5bdc243e48d69a
Ryan Ivers <75460163+RyanIvers@users.noreply.github.com>
2021-03-10 13:47:59 -0800

    Add files via upload

---
+++ React/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import FirstJsx from './components/FirstJsx/FirstJsx';
+import React from 'react';
+
+function App() {
+  return (
+    <>
+    <FirstJsx />
+    </>
+  );
+}
+
+export default App;
